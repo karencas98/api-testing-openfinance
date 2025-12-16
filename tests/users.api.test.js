f7e1d58b3290f8c7e56a52caf209947d485a6569
Karen Castañeda <157784167+karencas98@users.noreply.github.com>
2025-12-16 00:04:39 +0000

    Add API tests for users

--- tests/users.api.test.js
+++ tests/users.api.test.js
@@ -1,1 +1,21 @@
+const request = require('supertest');
+
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+describe('API Testing - Users', () => {
+
+  test('Should return list of users', async () => {
+    const response = await request(baseUrl).get('/users');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+
+  test('Should return error for invalid endpoint', async () => {
+    const response = await request(baseUrl).get('/user');
+
+    expect(response.status).toBe(404);
+  });
+
+});
 
